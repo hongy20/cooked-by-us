e4518d73f48d92c2e29e47818792cb4e1eb43744
Yan Hong <yan.hong.cn@gmail.com>
2025-11-12 14:37:46 +0100

    Add social sign-on as authentication method

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,24 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { getClient } from "./db/mongodb";
 
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+  throw new Error(
+    "Environment variables GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be set.",
+  );
+}
+
 const client = await getClient();
 
 export const auth = betterAuth({
   database: mongodbAdapter(client),
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    },
+  },
 });

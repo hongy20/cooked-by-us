5b734ae0d3c78c86efe594f7bac3ae0a8a7b5007
Yan Hong <yan.hong.cn@gmail.com>
2025-11-13 09:49:30 +0100

    Slightly better admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,11 @@ import { getSession } from "@/lib/auth";
 
 const Page = async () => {
   const session = await getSession();
-  return <p>Admin page with {session?.user.email}</p>;
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1>Admin page with {session?.user.email}</h1>
+    </main>
+  );
 };
 
 export default Page;

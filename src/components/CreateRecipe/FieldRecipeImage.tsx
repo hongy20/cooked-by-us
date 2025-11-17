c50eaa4ca0254731a709012163ed4a7353e0f1b3
Yan Hong <yan.hong.cn@gmail.com>
2025-11-17 16:28:42 +0100

    Limit file size

--- src/components/CreateRecipe/FieldRecipeImage.tsx
+++ src/components/CreateRecipe/FieldRecipeImage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Field, FieldDescription, FieldError, FieldLabel } from "../ui/field";
 import { Input } from "../ui/input";
 
@@ -8,8 +9,11 @@ interface Props {
 }
 
 const ID = "image";
+const FILE_LIMIT_IN_MB = 2;
 
 export const FieldRecipeImage = ({ name, defaultValue, error }: Props) => {
+  const [clientError, setClientError] = useState<string | null>(null);
+
   return (
     <Field>
       <FieldLabel htmlFor={ID}>Image</FieldLabel>
@@ -19,10 +23,24 @@ export const FieldRecipeImage = ({ name, defaultValue, error }: Props) => {
         type="file"
         accept="image/*"
         required={!defaultValue}
+        onChange={(e) => {
+          const file = e.target.files?.item(0);
+          if (
+            file instanceof File &&
+            file.size > FILE_LIMIT_IN_MB * 1024 * 1024
+          ) {
+            setClientError(`File must be smaller than ${FILE_LIMIT_IN_MB}MB`);
+            // 🔥 Reset file input
+            e.target.value = "";
+          } else {
+            setClientError(null);
+          }
+        }}
       />
       {defaultValue && (
         <FieldDescription>Image uploaded: {defaultValue}</FieldDescription>
       )}
+      <FieldError>{clientError}</FieldError>
       <FieldError>{error}</FieldError>
     </Field>
   );

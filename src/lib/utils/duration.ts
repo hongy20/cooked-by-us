72442bed5685de0edee4a14156a7953c73d80d87
Yan Hong <yan.hong.cn@gmail.com>
2025-11-18 10:02:34 +0100

    Handle potential undefined return from toHuman()

--- src/lib/utils/duration.ts
+++ src/lib/utils/duration.ts
@@ -6,11 +6,13 @@ export const minutesToISO = (durationInMinutes: number) =>
     minutes: durationInMinutes % 60,
   }).toISO(); // => "PT1H30M"
 
-export const minutesToHuman = (durationInMinutes: number) =>
-  Duration.fromObject({
+export const minutesToHuman = (durationInMinutes: number) => {
+  if (durationInMinutes < 0) return "Invalid duration";
+  return Duration.fromObject({
     hours: Math.floor(durationInMinutes / 60),
     minutes: durationInMinutes % 60,
   }).toHuman(); // => "1 hour, 30 minutes"
+};
 
 export const isoToMinutes = (iso: string) =>
   Duration.fromISO(iso).as("minutes");

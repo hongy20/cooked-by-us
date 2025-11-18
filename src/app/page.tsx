6bdab92ee9f47ace53bfa754b8f9c0d32cc93253
Yan Hong <yan.hong.cn@gmail.com>
2025-11-18 12:39:26 +0100

    Add JSON LD to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
+import type { ItemList, Recipe, WithContext } from "schema-dts";
 import { RecipeCard } from "@/components/RecipeCard";
 import { getAllRecipesAction } from "@/lib/action/recipe";
 
 export default async function Page() {
   const recipes = await getAllRecipesAction();
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const jsonLd: WithContext<ItemList> = {
+    "@context": "https://schema.org",
+    "@type": "ItemList",
+    itemListElement: recipes.map((recipe, index) => ({
+      "@type": "ListItem",
+      position: index + 1,
+      item: {
+        "@type": "Recipe",
+        name: recipe.name,
+        image: recipe.image,
+        recipeCuisine: recipe.cuisine,
+        recipeCategory: recipe.category,
+        keywords: recipe.keywords.join(", "),
+        url: `${BASE_URL}/recipe/${recipe._id}`,
+      } satisfies Recipe,
+    })),
+  };
+
   return (
     <div className="w-full flex flex-col items-center gap-4">
+      {/* Add JSON-LD to your page */}
+      <script
+        type="application/ld+json"
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: insert JSON-LD
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
+        }}
+      />
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
         All Recipes
       </h1>

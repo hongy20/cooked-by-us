c475bd2558a26d693b40a6ed403779a43e782f14
Yan Hong <yan.hong.cn@gmail.com>
2025-11-18 09:55:47 +0100

    Add error handling for database failures

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -3,7 +3,7 @@ import { getRecipe } from "@/lib/dal/recipe";
 
 export default async function Page({ params }: PageProps<"/recipe/[id]">) {
   const recipeId = await params.then(({ id }) => id);
-  const recipe = await getRecipe(recipeId);
+  const recipe = await getRecipe(recipeId).catch(console.error);
 
   if (!recipe) {
     notFound();

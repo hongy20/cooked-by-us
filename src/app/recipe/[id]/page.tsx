f97683ac0892d2b922d93f5b9d278daa2ebf5bad
Yan Hong <yan.hong.cn@gmail.com>
2025-11-19 12:26:16 +0100

    Add JSON LD to recipe page

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { RecipeJsonLd } from "@/components/RecipeJsonLd";
 import { RecipeView } from "@/components/RecipeView";
 import { SimilarRecipes } from "@/components/SimilarRecipes";
 import { getRecipe } from "@/lib/dal/recipe";
-
-// TODO: author name
-// TODO: json-ld
 
 type Props = PageProps<"/recipe/[id]">;
 
@@ -29,6 +27,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 export default async function Page({ params }: Props) {
   const recipeId = await params.then(({ id }) => id);
   const recipe = await getRecipe(recipeId).catch(console.error);
+  const authorName = "TBD";
 
   if (!recipe) {
     notFound();
@@ -36,7 +35,8 @@ export default async function Page({ params }: Props) {
 
   return (
     <div className="my-10 mx-12 md:mx-24 lg:mx-36">
-      <RecipeView recipe={recipe} authorName="" />
+      <RecipeJsonLd recipe={recipe} authorName={authorName} />
+      <RecipeView recipe={recipe} authorName={authorName} />
       <SimilarRecipes recipeId={recipeId} />
     </div>
   );

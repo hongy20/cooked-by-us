017c2183c2ec0ae60c0aafc8db9f0366476ab01a
Yan Hong <yan.hong.cn@gmail.com>
2025-11-20 11:54:43 +0100

    Add mongodb schema for category and cuisine

---
+++ src/lib/model/category.ts
@@ -0,0 +1,24 @@
+import { type Document, type Model, model, models, Schema } from "mongoose";
+
+export interface ICategory extends Document {
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const CategorySchema = new Schema<ICategory>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const CategoryModel =
+  (models.Category as Model<ICategory>) || model<ICategory>("Category", CategorySchema);

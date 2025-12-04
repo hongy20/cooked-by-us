4740eb56798629ce5ec55b54b68dfd67d5bcdddc
Yan Hong <yan.hong.cn@gmail.com>
2025-12-04 13:07:09 +0200

    Enable "speed insights" and "web analytics" (#20)
    * Enable speed insights
    
    * Enable web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Footer } from "@/components/Footer";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Header } from "@/components/Header/Header";
 import { Toaster } from "@/components/ui/sonner";
 import { COOKED_BY_US } from "@/lib/constant";
@@ -37,6 +39,8 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <SpeedInsights />
+        <Analytics />
         <Toaster />
         <Footer />
       </body>

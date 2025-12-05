fd8fb34ccffbb8819fbd6b5a481dfde2d48179c3
Yan Hong <yan.hong.cn@gmail.com>
2025-12-05 11:13:31 +0100

    Merge common logics regarding to deletion

---
+++ src/components/dashboard/EntityDeletionDialog.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { toast } from "sonner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  action: () => Promise<boolean>;
+  dialogTitle: string;
+  dialogDescription: string;
+  successMessage: string;
+  failureMessage: string;
+};
+
+export const EntityDeletionDialog = ({
+  action,
+  dialogTitle,
+  dialogDescription,
+  successMessage,
+  failureMessage,
+}: Props) => {
+  const [pending, startTransition] = useTransition();
+  const router = useRouter();
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="ghost" size="icon" className="text-red-600">
+          <Trash2 className="h-4 w-4" />
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{dialogTitle}</AlertDialogTitle>
+          <AlertDialogDescription>{dialogDescription}</AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel disabled={pending}>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={pending}
+            onClick={() =>
+              startTransition(
+                async () =>
+                  await action()
+                    .then((ok) => {
+                      if (ok) {
+                        toast.success(successMessage);
+                        router.refresh();
+                      } else {
+                        toast.error(failureMessage);
+                      }
+                    })
+                    .catch((error: Error) => {
+                      toast.error(failureMessage, {
+                        description: error.message,
+                      });
+                    }),
+              )
+            }
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
--- src/components/dashboard/category/DeleteCategoryButton.tsx
+++ src/components/dashboard/category/DeleteCategoryButton.tsx
@@ -1,70 +1,17 @@
 "use client";
 
-import { Trash2 } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
-import { toast } from "sonner";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
+import { EntityDeletionDialog } from "@/components/dashboard/EntityDeletionDialog";
 import { deleteCategoryAction } from "@/lib/action/category";
 
 type Props = { categoryId: string };
 
-export const DeleteCategoryButton = ({ categoryId }: Props) => {
-  const [pending, startTransition] = useTransition();
-  const router = useRouter();
-
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="ghost" size="icon" className="text-red-600">
-          <Trash2 className="h-4 w-4" />
-        </Button>
-      </AlertDialogTrigger>
-
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Delete Category</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. Are you sure you want to delete this
-            category?
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-
-        <AlertDialogFooter>
-          <AlertDialogCancel disabled={pending}>Cancel</AlertDialogCancel>
-          <AlertDialogAction
-            disabled={pending}
-            onClick={() =>
-              startTransition(
-                async () =>
-                  await deleteCategoryAction(categoryId)
-                    .then(() => {
-                      toast.success("Category deleted!");
-                      router.refresh();
-                    })
-                    .catch((error: Error) => {
-                      toast.error("Category deletion failed", {
-                        description: error.message,
-                      });
-                    }),
-              )
-            }
-          >
-            Delete
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
-};
+export const DeleteCategoryButton = ({ categoryId }: Props) => (
+  <EntityDeletionDialog
+    action={() => deleteCategoryAction(categoryId)}
+    dialogTitle="Delete Category"
+    dialogDescription="This action cannot be undone. Are you sure you want to delete this
+            category?"
+    successMessage="Category deleted!"
+    failureMessage="Category deletion failed"
+  />
+);
